bdd012b07661b2fb5ab61c8fe98ae48ff9a52422
Letsie <Pyroponya@gmail.com>
2025-11-21 18:22:48 +0300

    added connection to cloud storage for images

--- opinions_app/models.py
+++ opinions_app/models.py
@@ -8,6 +8,7 @@ class Opinion(db.Model):
     title = db.Column(db.String(128), nullable=False)
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
+    images = db.Column(db.JSON)
     timestamp = db.Column(
         db.DateTime,
         index=True,
@@ -21,11 +22,12 @@ class Opinion(db.Model):
             'title': self.title,
             'text': self.text,
             'source': self.source,
+            'images': self.images,
             'timestamp': self.timestamp,
             'added_by': self.added_by
         }
 
     def from_dict(self, data):
-        for field in ['title', 'text', 'source', 'added_by']:
+        for field in ['title', 'text', 'images', 'source', 'added_by']:
             if field in data:
                 setattr(self, field, data[field])
